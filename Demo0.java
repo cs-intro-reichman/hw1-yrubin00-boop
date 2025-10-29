dc164a5aebb9b291e24d16e3d64aa713a6548702
yrubin00-boop <yrubin00@gmail.com>
2025-10-29 17:45:32 +0200

    Update Demo0.java

--- Demo0.java
+++ Demo0.java
@@ -2,8 +2,15 @@
 // (Same program as shown in lecture 1-1).
 public class PrintTo5 {
 	public static void main(String[] args) {
-		// Declares an int variable, and sets it to 1
+		// Declares an int variable, and sets it to 0
         int i = 0;
-        // Complete the program's code here:
+        // כל עוד i קטן או שווה ל-5, המשך בלולאה
+        while (i <= 5) {
+            // הדפס את הערך הנוכחי של i, ואז עבור לשורה חדשה
+            System.out.println(i);
+            
+            // הגדל את i ב-1, כדי להתקדם לעבר תנאי העצירה
+            i = i + 1; // או בקיצור: i++;
+        }
     }
- }
+}
